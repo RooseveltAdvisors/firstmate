1e2a6c28d75a32ad710ef5b2fe37ecd8b1bdb5de
Jon Roosevelt <rooseveltadvisors@gmail.com>
2026-08-31 19:03:02 -0400

    no-mistakes(review): Reject partially quoted direct Beads commands

--- tests/fm-lint.test.sh
+++ tests/fm-lint.test.sh
@@ -755,7 +755,9 @@ SH
     'env -- bd close fm-example' \
     '/usr/local/bin/bd close fm-example' \
     '"/usr/local/bin/bd" close fm-example' \
-    "'/usr/local/bin/bd' close fm-example"
+    "'/usr/local/bin/bd' close fm-example" \
+    "b'd' close fm-example" \
+    "/usr/local/bin/b'd' close fm-example"
   do
     printf '#!/usr/bin/env bash\n%s\n' "$invocation" > "$tmp/repo/bin/direct-beads.sh"
     rc=0
